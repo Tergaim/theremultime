4b302c91ee5e405ab0e8bf70c6a8df2aa6388293
Tergaim <gp59155@gmail.com>
2019-11-06 15:59:00 +0100

    diveded project between cameras

---
+++ camerafront/camerafront.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	VideoCapture cap(0);
+	while(waitKey(20) <= 0){
+		Mat I;
+		cap >> I;
+		imshow("I",I);
+	}
+	return 1;
+}

4daa51d3cdbf9eb95d7d251d9c28feacf21a2799
pchangmaths0327 <pchangmaths0327@gmail.com>
2019-11-13 11:27:12 +0100

    Basic background substraction on front camera

--- camerafront/camerafront.cpp
+++ camerafront/camerafront.cpp
@@ -1,4 +1,9 @@
-#include <opencv2/highgui/highgui.hpp>
+#include <sstream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
 #include <iostream>
 #include <fstream>
 #include <cmath>
@@ -8,11 +13,16 @@ using namespace std;
 using namespace cv;
 
 int main() {
+	Ptr<BackgroundSubtractor> pBackSub;
+	pBackSub = createBackgroundSubtractorKNN();
+
 	VideoCapture cap(0);
+	Mat frame, fgMask;
 	while(waitKey(20) <= 0){
-		Mat I;
-		cap >> I;
-		imshow("I",I);
+		cap >> frame;
+		pBackSub -> apply(frame,fgMask);
+		imshow("fgMask",fgMask);
+		imshow("frame",frame);
 	}
 	return 0;
 }

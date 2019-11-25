94da0fce9fbe7df21990d60bf515810e4bb75a4c
pchangmaths0327 <pchangmaths0327@gmail.com>
2019-11-25 23:40:33 +0100

    Detecting blue object with bounding box

--- camerafront/camerafront.cpp
+++ camerafront/camerafront.cpp
@@ -19,66 +19,60 @@ int main() {
 	//pBackSub.setUpdateBackgroundModel(false);
 
 	VideoCapture cap(0);
-	Mat bgframe, frame, prevframe, grayframe, satframe, fgMask, edges, gabored;
-	Mat fgMaskR, fgMaskG, fgMaskB;
+	Mat bgframe, frame, prevframe, grayframe, satframe, fgMask, edges, colored;
 	Mat frameH, frameS, frameV;
 	cap >> bgframe;
-	int k;
+	int k, cutx;
 	while( (k = waitKey(20)) != 32){
 		if(k == 115){ //S key to set background reference
 			cap >> bgframe;
 			GaussianBlur(bgframe,bgframe,Size( 5, 5 ),1.0,0.0);
-			//normalize(bgframe,bgframe,0,1,NORM_MINMAX);
-			//bgframe -= mean(bgframe);
-			//bgframe = abs(bgframe);
 		}
 		cap >> frame;
 		GaussianBlur(frame,frame,Size( 5, 5 ),1.0,0);
-		//normalize(frame,frame,0,1,NORM_MINMAX);
-		//frame -= mean(frame);
-		//frame = abs(frame);
-		extractChannel(frame-bgframe, fgMaskB, 0);
-		extractChannel(frame-bgframe, fgMaskG, 1);
-		extractChannel(frame-bgframe, fgMaskR, 2);
-
-		threshold(fgMaskB,fgMaskB,10,255.0,THRESH_BINARY);
-		threshold(fgMaskG,fgMaskG,10,255.0,THRESH_BINARY);
-		threshold(fgMaskR,fgMaskR,10,255.0,THRESH_BINARY);
-		//pBackSub -> apply(frame,fgMask);
-		//GaussianBlur(fgMask,fgMask,Size( 25, 25 ),0,0);
-		cvtColor(frame-bgframe,grayframe,COLOR_BGR2GRAY);
-		fgMask = max(fgMaskR,max(fgMaskG,fgMaskB));
-		Canny(frame,edges,10.0,30.0,3);
+		pBackSub -> apply(frame,fgMask);
+		//cvtColor(frame-bgframe,grayframe,COLOR_BGR2GRAY);
 		cvtColor(frame, satframe,COLOR_BGR2HSV);
 		vector<Mat> hsvarray(3);
 		split(satframe, hsvarray);
 		hsvarray[1] *= 1.5;
 		merge(hsvarray, satframe);
-		//cvtColor(satframe,satframe, COLOR_HSV2BGR);
-		imshow("satframe", satframe);
-		//imshow("fgMaskR",fgMaskR);
-		//imshow("fgMaskG",fgMaskG);
-		//imshow("fgMaskB",fgMaskB);
-		imshow("canny",edges);
-		imshow("frame",frame);
-		//flip(frame,frame,1);
-		inRange(satframe, Scalar(90, 20, 0), Scalar(120, 200, 255), fgMask);
-		inRange(hsvarray[0], 90, 120, frameH);
-		inRange(hsvarray[1], 20, 200, frameS);
-		inRange(hsvarray[2], 0, 255, frameV);
-		//fgMask = min(min(frameH, frameS),frameV);
-		//bitwise_and(frame,frame, frame, fgMask);
-		imshow("fgMask",fgMask);
-		//next time : try hysteresis thresholding to get only hands
-		//Gabor filter for texture segmentation
-		Mat kernel1;
-		int kernel_size = 3;
-        double sig = 5, th = 0, lm = 8, gm = 0.02, ps = 0, theta=0;
-		kernel1 = cv::getGaborKernel(cv::Size(kernel_size,kernel_size), sig, theta, lm, gm, ps, CV_32F);
-		//grayframe.convertTo(grayframe,CV_32F);
-        //filter2D(frame, gabored, CV_32F, kernel1);
-		//imshow("grey", grayframe);
+		inRange(satframe, Scalar(90, 20, 100), Scalar(110, 200, 255), colored);
+		fgMask = min(fgMask,colored);
 
+		Mat Points, PointsL, PointsR;
+		findNonZero(fgMask,Points);
+		Rect Min_Rect=boundingRect(Points);
+
+		if (countNonZero(fgMask.col((Min_Rect.tl().x + Min_Rect.br().x)/2))>1){
+			cutx = (fgMask.cols + Min_Rect.tl().x + Min_Rect.br().x)/2 % fgMask.cols;
+		}else{
+			cutx = (Min_Rect.tl().x + Min_Rect.br().x)/2;
+		}
+		
+		Mat fgMaskL(fgMask, Rect(0,0,cutx,fgMask.rows-1));
+		findNonZero(fgMaskL,PointsL);
+		Rect Min_Rect_L = boundingRect(PointsL);
+
+		Mat fgMaskR(fgMask, Rect(cutx,0,fgMask.cols-1-cutx,fgMask.rows-1));
+		findNonZero(fgMaskR,PointsR);
+		Rect Min_Rect_R = boundingRect(PointsR)+ Point(cutx,0);
+
+		rectangle(frame,Min_Rect,Scalar(0,255,0),2);
+		rectangle(frame,Min_Rect_L,Scalar(255,0,0),2);
+		rectangle(frame,Min_Rect_R,Scalar(0,0,255),2);
+
+		//Canny(colored,edges,10.0,30.0,3);
+		//GaussianBlur(fgMask,fgMask,Size( 5, 5 ),1.0,0);
+		//cvtColor(satframe,satframe, COLOR_HSV2BGR);
+
+		
+		//imshow("satframe", satframe);
+		//imshow("canny",edges);
+		imshow("frame",frame);
+		//imshow("fgMask",fgMask);
+		//imshow("colored",colored);
+		//imshow("canny",edges);
 	}
 	return 0;
 }
